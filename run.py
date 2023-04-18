62ce5ec022c3f439199c0493f6bedc7588059559
Mon Petit Président <125287250+mon-petit-pr@users.noreply.github.com>
2023-04-18 21:19:38 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
+# pylint: import-error
+# This program is dedicated to the public domain under the CC0 license.
+
 from os import getenv
 from app.MPP import app
 
 def main() -> None:
     application = app()
-    application.run_webhook(
-        listen='0.0.0.0',
-        port=getenv('WEBHOOK_URL',8080),
-        webhook_url=getenv('WEBHOOK_URL')
-    )
-    #application.run_polling()
+    #application.run_webhook(
+    #    listen='0.0.0.0',
+    #    port=getenv('WEBHOOK_URL',8080),
+    #    webhook_url=getenv('WEBHOOK_URL')
+    #)
+    application.run_polling()
 if __name__ == "__main__":
     main()
