9ed1ac78a4b14940020aeea4cb86a8a84df88982
Mon Petit Président <125287250+mon-petit-pr@users.noreply.github.com>
2023-04-18 20:32:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from app.MPP import app
 
 def main() -> None:
     application = app()
-     application.run_webhook(
-         listen='0.0.0.0',
-         port=getenv('WEBHOOK_URL',8080),
-         webhook_url=getenv('WEBHOOK_URL')
-     )
+    application.run_webhook(
+        listen='0.0.0.0',
+        port=getenv('WEBHOOK_URL',8080),
+        webhook_url=getenv('WEBHOOK_URL')
+    )
     #application.run_polling()
 if __name__ == "__main__":
     main()
